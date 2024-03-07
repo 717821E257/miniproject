41d0a0ec6a8bca26d20c5d5ba2f15b2c284cca7d
Thenmozhi <136812529+717821E257@users.noreply.github.com>
2024-03-07 08:43:11 -0800

    Library.java

--- 717821E257_LIBRARY MANAGEMENT SYSTEM/src/com/kce/bean/Library.java
+++ 717821E257_LIBRARY MANAGEMENT SYSTEM/src/com/kce/bean/Library.java
@@ -1,5 +1,47 @@
 package com.kce.bean;
 
 public class Library {
+  private int BOOK_ID;
+	private String TITLE;
+	private String AUTHOR;
+	private String  NAME;
+	public Library(int BOOK_ID, String TITLE, String AUTHOR, String NAME) {
+	super();
+	this.BOOK_ID = BOOK_ID;
+	this.TITLE= TITLE;
+	this.AUTHOR = AUTHOR;
+	this.NAME = NAME;
+	}
+	public int getBOOK_ID() {
+	return BOOK_ID;
+	}
+	public void setBOOK_ID(int BOOK_ID) {
+	this.BOOK_ID = BOOK_ID;
+	}
+	public String getTITLE() {
+
+	return TITLE;
+	}
+	public void setTITLE(String TITLE) {
+	this.TITLE = TITLE;
+	}
+	public String getAUTHOR() {
+	return AUTHOR;
+	}
+	public void setAUTHOR(String AUTHOR) {
+	this.AUTHOR = AUTHOR;
+	}
+	public String getNAME() {
+		return NAME;
+	}
+	public void setNAME() {
+		this.NAME = NAME;
+	}
+	@Override
+	public String toString() {
+	return "Library [BOOK_ID="+ BOOK_ID + ",TITLE="+TITLE+",AUTHOR="+AUTHOR+",NAME="+NAME+"]";
 
 }
+}
+
+
