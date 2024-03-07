2657abb11bcf47552af769ff48d78e1b8c1a88bf
Thenmozhi <136812529+717821E257@users.noreply.github.com>
2024-03-07 08:49:22 -0800

    LibraryManagementSystem.java

--- 717821E257_LIBRARY MANAGEMENT SYSTEM/src/com/kce/service/LibraryManagementSystem.java
+++ 717821E257_LIBRARY MANAGEMENT SYSTEM/src/com/kce/service/LibraryManagementSystem.java
@@ -1,5 +1,65 @@
 package com.kce.service;
 
 public class LibraryManagementSystem {
+  public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+		boolean exit = false;
+		while (!exit) {
+		LibraryDao.option();
+		System.out.println("Enter your choice:");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+		LibraryDao.create();
+		break;
+		case 2:
+		System.out.println("Enter the number of books to insert:");
+		int noOfBooks = sc.nextInt();
+		LibraryDao.insert(noOfBooks);
+		break;
+		case 3:
+		LibraryDao.display();
+		break;
+
+		case 4:
+		System.out.println("Enter the books name to delete:");
+		String bookName = sc.next();
+		LibraryDao.delete(bookName);
+		break;
+		case 5:
+		LibraryDao.update();
+		break;
+		case 6:
+		LibraryDao.display2();
+		break;
+		case 7:
+		LibraryDao.truncateTable();
+		break;
+		case 8:
+		LibraryDao.deleteTable();
+		break;
+		default:
+		System.out.println("Invalid choice. Please enter a valid option.");
+		break;
+		}
+		System.out.println("Do you want to continue (Yes/No)?:");
+		String continueChoice = sc.next();
+		if (continueChoice.equalsIgnoreCase("No")) {
+		exit = true;
+		}
+		}
+		}
+		catch (ClassNotFoundException e) {
+		System.out.println(e);
+		} catch(SQLException e) {
+		System.out.println(e);
+		}
+		}
+}
+
+
+
+  
 
 }
